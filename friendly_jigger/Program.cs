a8ba088ecd18f1665b98a40c4a96f315085d0b17
Kevin Phillips <thefriendlycoder@gmail.com>
2021-01-09 17:31:25 -0400

    fixed dotnet 5.0 single file build

--- friendly_jigger/Program.cs
+++ friendly_jigger/Program.cs
@@ -1,5 +1,23 @@
 ﻿using System;
 
+/*
+ * In order to make this work with .NET 5.0 I had to enable 
+ * inclusion of native binaries which prevents certain 
+ * optimizations like the ready to run flag. This necessitates 
+ * having to extract the binary to a temp folder every time you 
+ * run it which slows startup times. The only solution is to use 
+ * .NET 3.1 instead. Bug is reported here: 
+ * https://github.com/dotnet/runtime/issues/36590
+ * 
+ * We could work around the problem by packaging the several files that
+ * are normally generated in .NET 5 into an installation package but we'd
+ * need to find a way to create platform specific packages and cross-compile
+ * those as well which may be hard.
+ * 
+ * Also, we can not do platform optimizations while running builds on the
+ * Travis CI docker environment for some reason. May need further investigation
+ * to see if there is a fix.
+ */
 namespace friendly_jigger
 {
     class Program
