da57ba8a528da449bebc4c9a989837fab5e4987f
Kevin Phillips <thefriendlycoder@gmail.com>
2021-01-09 14:44:43 -0400

    Add project files.

---
+++ friendly_jigger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace friendly_jigger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
